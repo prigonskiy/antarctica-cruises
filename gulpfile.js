e58eed70dfd4ed15d9639844833620c6402399a5
Filipp Prigonskiy <jerk1991@gmail.com>
2022-08-03 20:17:52 +0300

    Начал делать меню

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,14 @@ const refresh = (done) => {
   done();
 };
 
-const build = gulp.series(clean, svgo, copy, css, sprite, js);
+const createWebp = () => {
+  const root = '';
+  return gulp.src(`source/img/${root}**/*.{png,jpg}`)
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest(`source/img/${root}`));
+};
+
+const build = gulp.series(clean, svgo, createWebp, copy, css, sprite, js);
 
 const start = gulp.series(build, syncServer);
 
@@ -125,13 +132,6 @@ const start = gulp.series(build, syncServer);
 
 // root = '' - по дефолту webp добавляются и обналяются во всех папках в source/img/
 // root = 'content/' - webp добавляются и обновляются только в source/img/content/
-
-const createWebp = () => {
-  const root = '';
-  return gulp.src(`source/img/${root}**/*.{png,jpg}`)
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest(`source/img/${root}`));
-};
 
 const optimizeImages = () => {
   return gulp.src('build/img/**/*.{png,jpg}')
